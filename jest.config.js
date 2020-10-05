12acb0a6eea1f6ed21fbdc09bd2d8a3f6e37e1a0
Cristiano <Morgante>
2020-10-05 17:29:43 -0300

    Add resolve path to jest

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   testPathIgnorePatterns: ['/mode_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/']
 }
