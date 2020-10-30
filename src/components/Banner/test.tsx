1a802210b1215b719c133ca7df502e81f0934a3c
Cristiano <cristianomorgante@hotmail.com>
2020-10-29 23:44:35 -0300

    Add Banner component with storybook and tests

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import Banner from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1024x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render the heading', () => {
+    const { container } = renderWithTheme(<Banner {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Play the new/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
