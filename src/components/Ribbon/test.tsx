ff02957e50192d5309aa46a68f441cce1bfddf5d
Cristiano <cristianomorgante@hotmail.com>
2020-11-16 22:26:33 -0300

    Add Ribbon component with storybook and tests

---
+++ src/components/Ribbon/test.tsx
@@ -0,0 +1,45 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import Ribbon from '.'
+
+describe('<Ribbon />', () => {
+  it('should render the text correctly', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toBeInTheDocument()
+  })
+
+  it('should render the primary color', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      backgroundColor: '#f231a5'
+    })
+  })
+
+  it('should render the secondary color', () => {
+    renderWithTheme(<Ribbon color="secondary">Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      backgroundColor: '#3cd3c1'
+    })
+  })
+
+  it('should render with the normal size as default', () => {
+    renderWithTheme(<Ribbon color="secondary">Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      fontSize: '1.4rem',
+      height: '3.6rem'
+    })
+  })
+
+  it('should render with the small size', () => {
+    renderWithTheme(<Ribbon size="small">Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      fontSize: '1.2rem',
+      height: '2.6rem'
+    })
+  })
+})
