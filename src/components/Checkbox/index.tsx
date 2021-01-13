024ffe7507b580f728ed02f4eff2d08abe27396f
Cristiano <cristianomorgante@hotmail.com>
2021-01-12 22:24:53 -0300

    Add onChange on checkbox component

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,19 +1,36 @@
+import { InputHTMLAttributes, useState } from 'react'
 import * as S from './styles'
 
 export type CheckboxProps = {
   label?: string
   labelFor?: string
   labelColor?: 'white' | 'black'
-}
+  onCheck?: (status: boolean) => void
+} & InputHTMLAttributes<HTMLInputElement>
 
 const Checkbox = ({
   label,
   labelFor = '',
-  labelColor = 'white'
+  labelColor = 'white',
+  onCheck
 }: CheckboxProps) => {
+  const [checked, setChecked] = useState(false)
+  const onChange = () => {
+    const status = !checked
+    setChecked(status)
+
+    onCheck && onCheck(status)
+  }
+
   return (
     <S.Wrapper>
-      <S.Input type="checkbox" name={labelFor} id={labelFor} />
+      <S.Input
+        type="checkbox"
+        name={labelFor}
+        id={labelFor}
+        onChange={onChange}
+        checked={checked}
+      />
       {!!label && (
         <S.Label htmlFor={labelFor} labelColor={labelColor}>
           {label}
--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,4 +1,5 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import theme from 'styles/theme'
 import { renderWithTheme } from 'utils/tests/helpers'
 import Checkbox from '.'
@@ -25,6 +26,19 @@ describe('<Checkbox />', () => {
 
     expect(screen.getByText(/checkbox label/i)).toHaveStyle({
       color: theme.colors.black
+    })
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="checkbox" onCheck={onCheck} />)
+
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
     })
   })
 })
