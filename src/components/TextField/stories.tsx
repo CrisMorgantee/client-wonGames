3ee9a9e3503db718fbd4bcebf6c2dfcee788a5b5
Cristiano <cristianomorgante@hotmail.com>
2021-02-01 18:41:56 -0300

    Add Input component

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import { Email } from '@styled-icons/material-outlined'
+import TextField, { TextFieldProps } from '.'
+
+export default {
+  title: 'Form/TextField',
+  component: TextField,
+  args: {
+    label: 'E-mail',
+    labelFor: 'Email',
+    id: 'Email',
+    initialValue: '',
+    placeholder: 'cris@gmail.com'
+  },
+  argTypes: {
+    onInput: { action: 'changed' },
+    icon: { type: '' }
+  }
+} as Meta
+
+export const Default: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+export const WithIcon: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} icon={<Email />} />
+  </div>
+)
+
+export const withError: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+withError.args = {
+  error: 'Ops...something is wrong'
+}
