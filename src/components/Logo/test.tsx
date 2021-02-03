fbbaa878b7c775109e7b5b069deb9636a3b014a1
Cristiano <cristianomorgante@hotmail.com>
2021-02-03 18:52:57 -0300

    Add dinamic id to svg in logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -17,6 +17,12 @@ describe('<Logo />', () => {
     })
   })
 
+  it('should render the logo with id passed', () => {
+    const { container } = renderWithTheme(<Logo id="myId" />)
+
+    expect(container.querySelector('#paint_linear_myId')).toBeInTheDocument()
+  })
+
   it('should render a normal logo when size is default', () => {
     renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
